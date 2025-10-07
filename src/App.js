7db896e8167b61dafcb200181a70cb3b1d278975
kimseanbun <bunkimsean6@gmail.com>
2025-10-07 16:35:39 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Menu from "./components/Menu";
+import data from "./data/recipes.json";
+
+function App() {
+  return (
+    <Menu recipes={data} />
+  );
+}
+
+export default App;
+
